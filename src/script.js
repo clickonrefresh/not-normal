a321ec029f47bca1f7762dfb46282a308f7bdd7f
clickonrefresh <clickonrefresh@gmail.com>
2021-12-01 10:34:57 +0000

    added text for each mesh

--- src/script.js
+++ src/script.js
@@ -64,35 +64,6 @@ const scene = new THREE.Scene()
 
 
 
-/**
- * Fonts
- */
-const fontLoader = new FontLoader()
-
-fontLoader.load(
-    '/fonts/optimer_regular.typeface.json',
-    (font) =>
-    {
-        const textGeometry = new TextGeometry(
-            'First Material',
-            {
-                font,
-                size: 1,
-                height: 1,
-                curveSegments: 10,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 5,
-
-            }
-        )
-        const textMaterial = new THREE.MeshBasicMaterial()
-        const text = new THREE.Mesh(textGeometry, textMaterial)
-        scene.add(text)
-    }
-)
 
 
 /**
@@ -139,7 +110,7 @@ const torus = new THREE.Mesh(
 )
 torus.position.y = 1
 
-
+//
 
 
 const material1 = new THREE.MeshMatcapMaterial()
@@ -151,7 +122,7 @@ const torus1 = new THREE.Mesh(
 torus1.position.y = -1
 
 
-
+//
 
 const material3 = new THREE.MeshMatcapMaterial()
 material3.matcap = matcapTexture1
@@ -161,6 +132,8 @@ const plane1 = new THREE.Mesh(
 )
 plane1.position.z = -3
 
+//
+
 const material2 = new THREE.MeshNormalMaterial()
 const torus2 = new THREE.Mesh(
     new THREE.TorusGeometry(1.0, 0.4, 40, 132, 32),
@@ -168,12 +141,13 @@ const torus2 = new THREE.Mesh(
 )
 material2.wireframe = true
 torus2.position.z = 3
-
+//
 
 
 
 
 const material4 = new THREE.MeshDepthMaterial()
+const material4a = new THREE.MeshDepthMaterial()
 const sphere1 = new THREE.Mesh(
     new THREE.SphereGeometry(1, 150, 150),
     material4
@@ -181,7 +155,7 @@ const sphere1 = new THREE.Mesh(
 material4.wireframe = true
 // sphere1.position.x = -2
 sphere1.position.z = 9
-
+// OVER HERE LAST
 
 /**
  * Mesh Lambert Material - requires ambient lighting and point light, ie requires occlusion to look right. Has good performance but strange visual effect.
@@ -194,6 +168,9 @@ const torus3 = new THREE.Mesh(
 )
 
 torus3.position.z = 2
+
+// here now
+
 
 /**
  * Phong Material - just the opposite of Lambert, use phong for objects that need higher definition/quality than objects out of focus or not of focal value
@@ -209,7 +186,7 @@ const sphere2 = new THREE.Mesh(
 
 sphere2.position.z = 3.5
 
-
+// done
 
 /**
  * Toon Material - just the opposite of Lambert, use phong for objects that need higher definition/quality than objects out of focus or not of focal value
@@ -227,6 +204,8 @@ const torus4 = new THREE.Mesh(
 torus4.position.x = -1
 // torus4.position.z = 11
 
+
+// done
 
 /**
  * 
@@ -256,7 +235,20 @@ gui.add(material8, 'roughness').min(0).max(1).step(0.0001)
 gui.add(material8, 'aoMapIntensity').min(0).max(10).step(0.001)
 gui.add(material8, 'displacementScale').min(0).max(1).step(0.0001) //displace is also normal
 
-
+const material8a = new THREE.MeshStandardMaterial()
+material8a.metalness = 0.5
+material8a.roughness = 0.5
+material8a.map = doorColorTexture
+material8a.aoMap = doorAmbientOcclusionTexture
+material8a.aoMapIntensity = 1
+// material8a.displacementMap = doorHeightTexture
+// material8a.displacementScale = 0.05
+material8a.metalnessMap = doorMetalnessTexture
+material8a.roughnessMap = doorRoughnessTexture
+material8a.normalMap = doorNormalTexture
+// material8a.normalScale.set(0.5, 0.5)
+// material8a.transparent = true
+// material8a.alphaMap = doorAlphaTexture
 
 
 // const torus5 = new THREE.Mesh(
@@ -281,7 +273,7 @@ plane2.geometry.setAttribute(
     'uv2',
     new THREE.BufferAttribute(plane2.geometry.attributes.uv.array, 2))
 
-
+// here
 /**
  * Physical Material - provides clearcoat, more gpu intensive
  */
@@ -296,7 +288,7 @@ plane2.geometry.setAttribute(
  
  sphere3.position.z = 6.5
 
-
+// here
 
 /**
  * EnvironmentMap
@@ -329,10 +321,318 @@ const torus5 = new THREE.Mesh(
 )
 torus5.position.y = 1
 torus5.position.z = 15
+// done
+
+scene.add(sphere, sphere1, sphere2, sphere3, sphere4, plane, plane1, plane2, plane3, torus, torus1, torus2, torus3, torus4, torus5)
 
 
-scene.add(sphere, sphere1, sphere2, sphere3, sphere4, plane, plane1, plane2, plane3, torus, torus1, torus2, torus3, torus4, torus5,)
 
+/**
+ * Fonts
+ */
+ const fontLoader = new FontLoader()
+
+ fontLoader.load(
+     '/fonts/optimer_regular.typeface.json',
+     (font) =>
+     {
+
+         const textGeometry = new TextGeometry(
+             'Sphere Mesh Normal Material with SmoothShading',
+             {
+                font,
+                size: 0.07,
+                height: 0.01,
+                curveSegments: 4,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+ 
+             }
+         );
+         const textGeometry1 = new TextGeometry(
+            'Plane Mesh Normal Material with SmoothShading',
+            {
+                font,
+                size: 0.1,
+                height: 0.02,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 3,
+
+            }
+        );
+         const textGeometry2 = new TextGeometry(
+            'Torus Mesh Normal Material',
+            {
+                font,
+                size: 0.1,
+                height: 0.02,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 3,
+
+            }
+        )
+         const textGeometry3 = new TextGeometry(
+            'Torus Mesh Matcap Material',
+            {
+                font,
+                size: 0.1,
+                height: 0.02,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 3,
+
+            }
+        )
+         const textGeometry4 = new TextGeometry(
+            'Plane Mesh Matcap Material',
+            {
+                font,
+                size: 1,
+                height: 0.13,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 3,
+
+            }
+        )
+         const textGeometry5 = new TextGeometry(
+            'Torus Mesh Normal Material - Wireframe',
+            {
+                font,
+                size: 0.2,
+                height: 0.04,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry6 = new TextGeometry(
+            'Sphere Mesh Depth Material - Wireframe',
+            {
+                font,
+                size: 0.07,
+                height: 0.01,
+                curveSegments: 4,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry7 = new TextGeometry(
+            'Torus Mesh Lambert Material',
+            {
+                font,
+                size: 0.06,
+                height: 0.01,
+                curveSegments: 3,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry8 = new TextGeometry(
+            'Sphere Mesh Phong Material',
+            {
+                font,
+                size: 0.06,
+                height: 0.01,
+                curveSegments: 3,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry9 = new TextGeometry(
+            'Torus Mesh Toon Material',
+            {
+                font,
+                size: 0.07,
+                height: 0.01,
+                curveSegments: 4,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry10 = new TextGeometry(
+            'Plane Mesh Standard Material with maps',
+            {
+                font,
+                size: 0.075,
+                height: 0.015,
+                curveSegments: 5,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry11 = new TextGeometry(
+            'Sphere Mesh Physical Material with Clearcoat',
+            {
+                font,
+                size: 0.075,
+                height: 0.015,
+                curveSegments: 5,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometry12 = new TextGeometry(
+            ' Environment Maps',
+            {
+                font,
+                size: 0.09,
+                height: 0.02,
+                curveSegments: 5,
+                bevelEnabled: false,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+         const textGeometryLink = new TextGeometry(
+            'Visit Me',
+            {
+                font,
+                size: 0.7,
+                height: 0.04,
+                curveSegments: 5,
+                bevelEnabled: true,
+                bevelThickness: 0.001,
+                bevelSize: 0.01,
+                bevelOffset: 0,
+                bevelSegments: 2,
+
+            }
+        )
+        
+
+        //  const textMaterial = new THREE.MeshBasicMaterial()
+        //  textMaterial.normalMap = true
+         const text = new THREE.Mesh(textGeometry, material)
+         text.position.x = 2.7
+         textGeometry.center()
+
+
+         const text1 = new THREE.Mesh(textGeometry1, material)
+         text1.position.z = -2
+         textGeometry1.center()
+
+
+         const text2 = new THREE.Mesh(textGeometry2, material)
+         text2.position.y = 1
+         textGeometry2.center()
+
+
+         const text3 = new THREE.Mesh(textGeometry3, material1)
+         text3.position.y = -2
+         textGeometry3.center()
+
+
+
+         const text4 = new THREE.Mesh(textGeometry4, material3)
+         text4.position.z = -2.95
+         text4.position.y = -5
+         textGeometry4.center()
+
+
+         const text5 = new THREE.Mesh(textGeometry5, material2)
+         text5.position.z = 3.5
+         text5.position.y = 1
+         textGeometry5.center()
+
+
+         const text6 = new THREE.Mesh(textGeometry6, material4a)
+         text6.position.z = 9
+        //  text6.position.y = 1
+         textGeometry6.center()
+
+
+         const text7 = new THREE.Mesh(textGeometry7, material5)
+         text7.position.z = 2.9
+        //  text6.position.y = 1
+         textGeometry7.center()
+
+
+         const text8 = new THREE.Mesh(textGeometry8, material6)
+         text8.position.z = 4
+        //  text6.position.y = 1
+         textGeometry8.center()
+
+
+         const text9 = new THREE.Mesh(textGeometry9, material7)
+         text9.position.x = - 2.3
+         textGeometry9.center()
+
+
+         const text10= new THREE.Mesh(textGeometry10, material8a)
+         text10.position.z =  11.3
+         textGeometry10.center()
+
+
+         const text11= new THREE.Mesh(textGeometry11, material9)
+         text11.position.z =  7.1
+         textGeometry11.center()
+
+
+         const text12= new THREE.Mesh(textGeometry12, material10)
+         text12.position.z =  15
+         textGeometry12.center()
+ 
+         const textLink = new THREE.Mesh(textGeometryLink, material3)
+         textLink.position.y = -8
+        //  textLink.position.x = -8
+         textLink.position.z = -1.5
+         textGeometryLink.center()
+
+         // text.rotation.y = 0.1 rotation here only affexcts the positioning
+         scene.add(text, text1, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12, textLink )
+ 
+     }
+ )
+ 
 
 
 /**
@@ -415,7 +715,7 @@ const clock = new THREE.Clock()
 const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
-    // Update Obkects
+    // Update Objects
     sphere.rotation.y = 0.1 * elapsedTime
     sphere.rotation.x = 0.17 * elapsedTime
     sphere1.rotation.y = 0.17 * elapsedTime
@@ -428,6 +728,8 @@ const tick = () => {
     torus2.rotation.z = -0.16 * elapsedTime
     // torus2.rotation.z = 0.12 * elapsedTime
     torus4.rotation.x = 0.09 * elapsedTime
+    // text.rotation.x = 0.09 * elapsedTime
+
 
 
     // Update controls
